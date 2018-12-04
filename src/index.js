2bba78efb4fee8cf295eb1a9562f35b10239a9c9
cymruhangu <pragquix@gmail.com>
2018-12-04 13:13:13 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import gameStore from './store';
 import './reset.css';
 import './index.css';
 
 import Game from './components/game';
 
 ReactDOM.render(
-  <Game />,
+  <Provider store={gameStore}>
+    <Game />
+  </Provider>,
   document.getElementById('root')
 );
